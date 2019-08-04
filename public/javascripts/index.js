3cb5d742fa269a316c05de823be090b2799257b8
poisenmj <maksjl01@gmail.com>
2019-08-04 13:25:06 +0100

    Update 1.4

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,10 @@ var nav_links = document.getElementsByClassName('nav-link');
 var pages = document.getElementsByClassName('pages');
 var pageData = document.getElementById('page-data').innerHTML;
 var current_page = pageData != '' ? pageData : 'About';
-document.getElementById('scrolldowna').href = '#'+current_page;
+
+window.onbeforeunload = function(evt){
+    console.log("Are you sure?");
+}
 
 //make scroll offset 0 insteaf of 9 pixels up which is the default
 zenscroll.setup(null, 0);
@@ -10,8 +13,10 @@ zenscroll.setup(null, 0);
 //set autoscroll to div when clicking from author page back to main page
 var scroll = document.getElementById('scrollbool').innerHTML;
 if(scroll == 'true'){
-    console.log(document.getElementById(current_page).getBoundingClientRect().top);
-    zenscroll.toY(document.documentElement.clientHeight);
+    //using set timout because it was not scrolling when going back with no delay
+    setTimeout(function(){
+        zenscroll.toY(document.documentElement.clientHeight);
+    }, 200)
 }
 
 //setup set bar under nav link and only show currently chosen div
@@ -29,7 +34,7 @@ for(var i = 0; i < pages.length; i++){
     else pages[i].style.display = '';
 }
 
-//functions for buttons
+//functions for twitter div
 function social(){
     var dark = document.getElementById('darken');
     document.getElementById('twitter').style.display = 'block';
@@ -51,12 +56,18 @@ function changeImageBack(div){
     div.getElementsByClassName('author-name')[0].style.opacity = '0';
 }
 
+function scrollup(){
+    zenscroll.toY(0);
+}
+function scrolldown(){
+    zenscroll.toY(document.documentElement.clientHeight);
+}
+
 function choose_page(page_name){
     for(var i = 0; i < pages.length; i++){
         pages[i].style.display = 'none';
     }
     document.getElementById(page_name).style.display = ''; 
-    document.getElementById('scrolldowna').href = '#'+page_name;
     current_page = page_name;
     zenscroll.to(document.getElementById(page_name));
 
